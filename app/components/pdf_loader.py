7595a539183b5ae3bbff81a83ba60071b8d7bf35
Your Name <you@example.com>
2025-08-09 17:18:51 -0400

    PDF Loader

---
+++ app/components/pdf_loader.py
@@ -0,0 +1,32 @@
+import os
+import pypdf
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from app.common.logger import get_logger
+from app.common.custom import CustomException
+
+from app.config.config import CHUNK_SIZE, CHUNK_OVERLAP, DATA_PATH
+
+logger = get_logger(__name__)
+
+
+def load_pdf_files():
+    try:
+        if not os.path.exists(DATA_PATH):
+            raise CustomException(f"Data path '{DATA_PATH}' does not exist.")
+        
+        logger.info(f"Loading PDF files from directory: {DATA_PATH}")
+        
+        loader = DirectoryLoader(path=DATA_PATH, glob="**/*.pdf", loader_cls=PyPDFLoader)
+        
+        documents = loader.load()
+        if not documents:
+            raise CustomException("No PDF documents found in the specified directory.")
+        logger.info(f"Loaded {len(documents)} PDF documents.")
+        
+        return documents
+    except Exception as e:
+        logger.error(f"Error loading PDF files: {e}")
+        raise CustomException("Failed to load PDF files.", e)
+        return []
